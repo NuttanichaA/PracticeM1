035e0ac27ca7995c8f5b0975d3fb3217241b943c
NuttanichaA <nuttanicha.auka@kmutt.ac.th>
2024-09-10 22:19:43 +0700

    Practice for Midterm INT20101

---
+++ calculateSales/ReadMe.md
@@ -0,0 +1,16 @@
+### Practice 7: Calculate Total Sales
+## Instruction:
+Write a function called calculateSales that takes an array of product objects as a parameter. Each product object contains:
+    name (string): The product's name.
+    price (number): The price of the product.
+    quantity (number): The number of units sold.
+The function should return the total sales (price * quantity) for all products.
+## Example: 
+const products = [
+  { name: 'Laptop', price: 1000, quantity: 5 },
+  { name: 'Phone', price: 500, quantity: 10 },
+  { name: 'Tablet', price: 300, quantity: 8 }
+];
+
+calculateSales(products);
+- Expected output: 12400
---
+++ calculateSales/calculateSales.js
@@ -0,0 +1,37 @@
+function calculateSales(products) {
+  if (!Array.isArray(products) || products.length === 0)
+    throw new Error("No products in this list!");
+//   for (item of products) {
+//     const totalSales = item.price * item.quantity;
+//     sumOfSales += totalSales;
+//   }
+ return products.reduce((sumOfSales, currentValue) => 
+    {return sumOfSales + (currentValue.price * currentValue.quantity)}, 0)
+}
+// when using an arrow function without curly braces ({}), you get an implicit return—which means 
+// the value is automatically returned from the function without needing an explicit return keyword. 
+// However, when you use curly braces to define the body of the function, you must use the return 
+// statement to explicitly return a value. Otherwise, the function will return undefined.
+
+const products = [
+  { name: "Laptop", price: 1000, quantity: 5 },
+  { name: "Phone", price: 500, quantity: 10 },
+  { name: "Tablet", price: 300, quantity: 8 },
+];
+
+console.log(calculateSales(products));
+try {
+  console.log(calculateSales(null));
+} catch (Error) {
+  console.log(Error);
+}
+try {
+  console.log(calculateSales(undefined));
+} catch (Error) {
+  console.log(Error);
+}
+try {
+  console.log(calculateSales([]));
+} catch (Error) {
+  console.log(Error);
+}
