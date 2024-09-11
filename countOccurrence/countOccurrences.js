1c465954ec370e477d97583516ad02db5ac5976b
NuttanichaA <nuttanicha.auka@kmutt.ac.th>
2024-09-11 13:42:27 +0700

    countOccurence function

---
+++ countOccurrence/countOccurrences.js
@@ -0,0 +1,15 @@
+function countOccurrences(words){
+    const occuredWords = {}
+    for (item of words) {
+        //this is implicit type conversion if item is not assign to ocw property yet, 
+        //it will be undefined which return false
+        if(occuredWords[item]) occuredWords[item]++
+        else occuredWords[item] = 1
+    }
+    return occuredWords
+}
+
+const words = ['apple', 'orange', 'apple', 'banana', 'apple'];
+const words1 = ['apple', 'banana', 'apple', 'banana', 'apple'];
+console.log(countOccurrences(words))
+console.log(countOccurrences(words1))
