26f2e7e10fbf6db198c52c8fdb4a643114e7631b
NuttanichaA <nuttanicha.auka@kmutt.ac.th>
2024-09-11 15:17:09 +0700

    add findLongestNameVer2

--- findLongestName/findLongestName.js
+++ findLongestName/findLongestName.js
@@ -10,11 +10,27 @@ function findLongestName(people) {
   return longestName
 }
 
+function findLongestNameVer2(people) {
+  let longestNames = [];
+
+  // First, find the maximum name length
+  let maxNamelength = Math.max(...people.map(person => person.name.length));
+
+  // Then, find all names with the maximum length
+  for (const person of people) {
+    if (person.name.length === maxNamelength) {
+      longestNames.push(person.name);
+    }
+  }
+
+  return longestNames;
+}
+
 const people = [
   { name: "Alice" },
   { name: "Bob" },
   { name: "Christopher" },
-  { name: "David" },
+  { name: "Aleczandria" },
 ];
 
-console.log(findLongestName(people));
+console.log(findLongestNameVer2(people));
